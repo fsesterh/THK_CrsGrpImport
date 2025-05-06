b4dfb11b9d231e9a6ddc6d4a8845b2212d40c88a
Marvin Beym <mBeym@databay.de>
2025-05-06 15:13:32 +0200

    Fix wrong max version

--- plugin.php
+++ plugin.php
@@ -9,7 +9,7 @@ $version = '9.0.0';
 // ilias min and max version; must always reflect the versions that should
 // run with the plugin
 $ilias_min_version = '9';
-$ilias_max_version = '9';
+$ilias_max_version = '9.999';
 
 // optional, but useful: Add one or more responsible persons and a contact email
 $responsible = 'Fabian Sesterhenn';
