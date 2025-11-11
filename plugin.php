5c378296ce30825d03d19337df5e69c84cf923cc
Fabian Sesterhenn <fabian.sesterhenn@th-koeln.de>
2025-11-11 09:06:27 +0100

    version 9-1-0: member gallery support
    updated example files, updated change-log, bumped version number due to
    new feature

--- plugin.php
+++ plugin.php
@@ -4,7 +4,7 @@
 $id = 'crsgrpimport';
 
 // code version; must be changed for all code changes
-$version = '9.0.1';
+$version = '9.1.0';
 
 // ilias min and max version; must always reflect the versions that should
 // run with the plugin
