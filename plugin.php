ad565b41516dad03e933fe0e5cdb3e6069f8b3e3
Fabian Sesterhenn <fabian.sesterhenn@th-koeln.de>
2026-06-24 15:03:49 +0200

    v9.1.1

--- plugin.php
+++ plugin.php
@@ -4,7 +4,7 @@
 $id = 'crsgrpimport';
 
 // code version; must be changed for all code changes
-$version = '9.1.0';
+$version = '9.1.1';
 
 // ilias min and max version; must always reflect the versions that should
 // run with the plugin
